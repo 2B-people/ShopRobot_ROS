60faa96c011d179a2d5501482725624aac77e6ca
2B-people <nqq0915@163.com>
2018-10-10 19:36:45 +0800

    Done shop_pid_cfg!

--- shoprobot/shop_pid/src/shop_pid_cfg.cpp
+++ shoprobot/shop_pid/src/shop_pid_cfg.cpp
@@ -3,50 +3,48 @@
 
 #include "shop_msgs/PID.h"
 
-#include <dynamic_reconfigure/server,h>
+#include <dynamic_reconfigure/server.h>
 #include <shop_pid/shopPIDConfig.h>
 
-double p,i,d;
+double p, i, d;
 
-void Callback(shop_pid::shopPIDConfig &config,double level)
+void Callback(shop_pid::shopPIDConfig &config, int level)
 {
     p = config.p;
     i = config.i;
     d = config.d;
 
     //echo P,I,D
-    
-    for(int i = 0; i < 3; i++)
-    {
-        ROS_INFO("NEW set: P: %f; I: %f; D: %f;", p, i, d);  
-    }
-    
+    ROS_INFO("NEW set!");
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "shop_pid");
+
+    ROS_INFO("Please use rqt_reconfigure to change pid");
+
+    ros::NodeHandle nh;
+    ros::NodeHandle pn("~");
+
+    int rate;
+    pn.param("p", p, 3.0);
+    pn.param("i", i, 0.3);
+    pn.param("d", d, 0.3);
+    pn.param("rate", rate, 10);
 
     dynamic_reconfigure::Server<shop_pid::shopPIDConfig> server;
     dynamic_reconfigure::Server<shop_pid::shopPIDConfig>::CallbackType cb;
     cb = boost::bind(&Callback, _1, _2);
-    Server.setCallback(cb);
+    server.setCallback(cb);
 
     shop_msgs::PID msg;
-    int rate;
 
-    ros::NodeHandle pn("~");
-    pn.param("p",p,3.0);
-    pn.param("i",i,0.3);
-    pn.param("d",d,0.3);
-    pn.param("rate",rate,10);
-
-    ros::Publisher pub = nh.advertise<shop_msgs::PID>("pid",10);
+    ros::Publisher pub = nh.advertise<shop_msgs::PID>("pid", 10);
 
     ros::Rate r(rate);
 
-    
-    while(nh.ok())
+    while (nh.ok())
     {
         msg.p = p;
         msg.i = i;
@@ -57,7 +55,6 @@ int main(int argc, char const *argv[])
         ros::spinOnce();
         r.sleep();
     }
-    
 
     return 0;
 }
