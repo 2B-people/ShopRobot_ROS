cb961e256388cfc6c7442177baeb5de48c98d286
2B-people <nqq0915@163.com>
2018-11-26 20:32:44 +0800

    done mcu_serial_test

--- BUGLIST.md
+++ BUGLIST.md
@@ -1,10 +1,13 @@
 # BUG_LIST
 
+`bug0001`
 2018.11.21:  main_interface中的MAIN需要在namespace外使用，不然编译时会出现
 ```shell
 /usr/lib/gcc/x86_64-linux-gnu/5/../../../x86_64-linux-gnu/crt1.o：在函数‘_start’中：
 (.text+0x20)：对‘main’未定义的引用
 
 ```
-
+`bug0002`
 2018.11.21: cmakelist中的library的名字需要注意，编译时会出现找不到对应library
+`bug0003`
+2018.11.25: main函数不能放在namespace中，会出现main函数找不到的情况，同
--- modules/driver/mcu_serial/src/mcu_serial_test.cpp
+++ modules/driver/mcu_serial/src/mcu_serial_test.cpp
@@ -1,8 +1,57 @@
-#include <mcu_serial.h>
+#include <stdint.h>
 
+#include <data/SerialTest.h>
+#include <ros/ros.h>
+#include <serial/serial.h>
 
-int main(int argc, char **argv)
-{
-    
-    return 0;
+#define USBPIN "/dev/ttyUSB0"
+#define USBBT 115200
+
+serial::Serial ser;
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "serial_example_node");
+  ros::NodeHandle nh;
+
+  ros::Publisher pub = nh.advertise<data::SerialTest>("vel_x", 10);
+
+  try {
+    ser.setPort(USBPIN);
+    ser.setBaudrate(USBBT);
+    serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+    ser.setTimeout(to);
+    ser.open();
+  } catch (const std::exception &e) {
+    ROS_ERROR("Unable to open port");
+    return -1;
+  }
+
+  while (ser.isOpen() == 0) {
+    ROS_INFO("Serial Init");
+  }
+
+  while (1) {
+    if (ser.available()) {
+      uint8_t hand[2];
+      ser.read(hand, 2);
+      if (hand[0] == 0x89 && hand[1] == 0x89) {
+        uint8_t data[8];
+        ser.read(data, 8);
+        if (data[0] == 0x00 && data[1] == 0x01)
+          for (int i = 2; i < 8; i++) {
+            ROS_INFO("read : %d", data[i]);
+            data::SerialTest msg;
+            for (int i = 0; i < 8; i++) {
+              msg.data[i] = data[i];
+            }
+
+            pub.publish(msg);
+          }
+        else
+          ROS_INFO("not is msg");
+      }
+    }
+    ros::spinOnce();
+  }
+  return 0;
 }
